4fdf3b6c978b35b2f8215a3ee18d450e1976fc2d
geguofeng <849447894@qq.com>
2018-02-10 01:01:30 +0800

    集成ElasticSearch client

--- src/main/java/com/gegf/Application.java
+++ src/main/java/com/gegf/Application.java
@@ -2,11 +2,14 @@ package com.gegf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
-public class DemoApplication {
+@RestController
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
---
+++ src/main/java/com/gegf/MyConfig.java
@@ -0,0 +1,28 @@
+package com.gegf;
+
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.InetAddress;
+
+@Configuration
+public class MyConfig {
+
+    @Bean
+    public TransportClient client() throws Exception{
+        InetSocketTransportAddress node = new InetSocketTransportAddress(
+                InetAddress.getByName("localhost"), 9300
+        );
+
+        Settings settings = Settings.builder()
+                .put("cluster.name", "gegf").build();
+
+        TransportClient client = new PreBuiltTransportClient(Settings.EMPTY);
+        client.addTransportAddress(node);
+        return client;
+    }
+}
